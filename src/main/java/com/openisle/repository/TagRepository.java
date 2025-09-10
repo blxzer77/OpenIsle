3157bbecbb0c9e850a9c03a5c426328e0c08df34
blxzer77 <blxzer1008@gmail.com>
2025-09-10 15:35:35 +0800

    添加了标签仓库接口

---
+++ src/main/java/com/openisle/repository/TagRepository.java
@@ -0,0 +1,17 @@
+package com.openisle.repository;
+
+import com.openisle.model.Tag;
+import com.openisle.model.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findByNameContainingIgnoreCase(String keyword);
+    List<Tag> findByApproved(boolean approved);
+    List<Tag> findByApprovedTrue();
+    List<Tag> findByNameContainingIgnoreCaseAndApprovedTrue(String keyword);
+
+    List<Tag> findByCreatorOrderByCreatedAtDesc(User creator, Pageable pageable);
+    List<Tag> findByCreator(User creator);
+}
